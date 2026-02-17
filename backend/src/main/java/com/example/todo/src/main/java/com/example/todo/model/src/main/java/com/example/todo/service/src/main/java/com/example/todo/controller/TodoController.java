87304b8dd2cc62fd79ed87811e97165d13b0134e
keerthika011 <etlakeerthi@gmail.com>
2026-02-17 23:02:12 +0530

    Added Todo controller layer

---
+++ backend/src/main/java/com/example/todo/src/main/java/com/example/todo/model/src/main/java/com/example/todo/service/src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,40 @@
+package com.example.todo.controller;
+
+import com.example.todo.model.Todo;
+import com.example.todo.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@CrossOrigin(origins = "*")
+public class TodoController {
+
+    private final TodoService service;
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Todo create(@RequestBody Todo todo) {
+        return service.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> getAll(@RequestParam(required = false) Boolean completed) {
+        return service.getAll(completed);
+    }
+
+    @PutMapping("/{id}")
+    public Todo update(@PathVariable Long id,
+                       @RequestBody Todo todo) {
+        return service.update(id, todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
